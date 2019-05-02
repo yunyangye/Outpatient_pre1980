9034f6d36e68e9ea0f6a21a61e61fc1c9c312fe1
satishupadhyaya <satishrajupadhyaya@gmail.com>
2019-05-02 11:16:54 -0600

    separate jobs

---
+++ 4C/4C.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=12
+#SBATCH --time=06-23
+#SBATCH --partition=shas
+#SBATCH --qos=long
+#SBATCH --output=sample-%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=saup0513
+
+module load python
+module load R
+python main.py
+
+module unload python
+module unload R
